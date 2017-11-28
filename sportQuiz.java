9cc5994f22e4bd527e3f971beed79286ca354160
nahomhg <nahomhaile_19@outlook.com>
2017-11-28 08:40:15 +0000

    Add files via upload

--- sportQuiz.java
+++ sportQuiz.java
@@ -7,36 +7,44 @@ import java.util.*; // input method
 
 public class sportQuiz {
     public static void main(String[]args) {
-        Teams[] manUnited = new Teams[3];
+        boolean start = startQuiz();
+        Teams[] manUnited = new Teams[4];
+        while(start) {
+
+            menu();
+            int catagoryChoice = userInput(); //takes user input and puts it in variable
+            quiz(catagoryChoice, manUnited); //quiz method takes variable.
+
+            System.out.println("Would you like to continue with the program? (Y/N)");
+            String isContinue = strInput();
+            if (isContinue.equalsIgnoreCase("y")) {
+                start = startQuiz();
+            } else {
+                System.out.println("Thank you for using the program");
+                System.exit(0);
+                break;
+            }
+        }
+    }
+    public static boolean startQuiz()
+    {
         boolean start = false;
+
         System.out.println("Welcome to the sports quiz! Enter 1 to begin: ");
         int response = userInput();
         if (response == 1) {
-            start = true;
+            return true;
         }
-        while (start) {
-            menu(); //goes to menu method
-
-            int catagoryChoice = userInput(); //takes user input and puts it in variable
-            quiz(catagoryChoice, manUnited); //quiz method takes variable.
-            System.out.println("Would youl like to continue with the program? (Y/N)");
-            String isContinue = strInput();
-            if (isContinue.equalsIgnoreCase("y")) {
-                menu();
-            } else {
-                System.out.println("Thank you for using thr program");
-                System.exit(0);
-            }
-            break;
-        }
+        return false;
     }
+
     public static void menu()
     {
         System.out.println("Chose from the following 2 catagories:\n ");
         System.out.println("1 - Cups and Wins.");
         System.out.println("2 - Most Goal Scored.");
-        System.out.println("3 - Most Appearances.");
-        System.out.println("4 - Most Spent on player");
+        System.out.println("3 - Most Spent on player");
+        System.out.println("4 - Most Appearances");
 
     }
     public static int quiz(int catagoryChoice, Teams[] team)
@@ -54,7 +62,7 @@ public class sportQuiz {
             else
                 System.out.println("Incorrect!");
         }
-        if(catagoryChoice == 2)
+        else if(catagoryChoice == 2)
         {
             boolean isRightCat2 = catagory2();
             if(isRightCat2)
@@ -63,59 +71,102 @@ public class sportQuiz {
                 score++;
                 System.out.println("Your current score: "+score);
             }
+            else
+                System.out.println("Incorrect");
+        }
+        else if(catagoryChoice == 3)
+        {
+        boolean isRightCat3 = catagory3();
+        if(isRightCat3)
+        {
+            System.out.println("You're correct!");
+            score++;
+            System.out.println("Your current score: "+score);
+        }
+        else
+            System.out.println("Incorrect");
+        }
+        else if(catagoryChoice == 4)
+        {
+            boolean isRightCat4 = catagory4();
+            if(isRightCat4)
+            {
+                System.out.println("You're correct!");
+                score++;
+                System.out.println("Your current score: "+score);
+            }
+            else
+                System.out.println("Incorrect!");
         }
         return score;
     }
     public static boolean catagory1()
     {
-        Teams[] manUnited = new Teams[3];
+        Teams[] manUnited = new Teams[4];
         System.out.println("How many FA cups have manchester united won?: ");
         int cupsAnswer = userInput();
         answers(manUnited);
         int score = 0;
         if(cupsAnswer == getFaCups(manUnited[0]))
         {
+            System.out.println(manUnited[0]);
             return true;
 
         }
-        else
-            System.out.println("You're incorrect!");
-
         return false;
     }
     public static boolean catagory2()
     {
-        Teams[] manUnited = new Teams[3];
-        System.out.println("What's the most goals score and who scored them?");
+        Teams[] manUnited = new Teams[4];
+        System.out.println("What's the most goals score?");
         int mostGoals = userInput();
-        String scoredBy = strInput();
+        //String scoredBy = strInput();
         answers(manUnited);
-        int score = 0;
-        if(mostGoals == getMostGoalScored(manUnited[1]) && scoredBy == getMostGoalScorer(manUnited[1]))
+        if(mostGoals == getMostGoalScored(manUnited[1]) )//)
         {
             return true;
             /* score = 2;
             System.out.println(score);*/
         }
-        else
-            System.out.println("You're incorrect!");
 
         return false; //return score;
     }
+
+    public static boolean catagory3(){
+        Teams[] manUnited = new Teams[4];
+        System.out.println("Who has been bought for the most amount?: ");
+        String mostPaid = strInput();
+        answers(manUnited);
+        if(mostPaid == getMostPaidForPlayer(manUnited[2]))
+        {
+            return true;
+        }
+        return false;
+    }
+    public static boolean catagory4(){
+        Teams[] manUnited = new Teams[4];
+        System.out.println("Who is the player that has the most appearances?: ");
+        String mostAppearance = strInput();
+        answers(manUnited);
+        if(mostAppearance == getPlayerMostAppearances(manUnited[3]))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public static void answers(Teams[] manUntd)
     {
         manUntd[0] = new Teams();
         manUntd[1] = new Teams();
         manUntd[2] = new Teams();
-        //manUntd[3] = new Teams();
+        manUntd[3] = new Teams();
 
 
         manUntd[0] = setFaCups(manUntd[0],12);
         manUntd[1] = setMostGoalScored(manUntd[1], 963);
-        manUntd[1] = setMostGoalScorer(manUntd[1], "Ryan Giggs");
-        manUntd[2] = setMostPaidForPlayer(manUntd[2],80000000);
         manUntd[2] = setmostExpensiveTransferedPlayer(manUntd[2],"Paul Pogba");
-        //manUntd[3] = setMostGoalScorer(manUntd[3],"Ryan Giggs");
+        manUntd[3] = setPlayerMostAppearances(manUntd[3],"Ryan Giggs");
 
     }
 
@@ -137,11 +188,11 @@ public class sportQuiz {
         return m;
     }
 
-    public int getMostPaidForPlayer(Teams m) {
+    public static String getMostPaidForPlayer(Teams m) {
         return m.mostPaidForPlayer;
     }
 
-    public static Teams setMostPaidForPlayer(Teams m,int mostPaidForPlayer) {
+    public static Teams setMostPaidForPlayer(Teams m,String mostPaidForPlayer) {
         m.mostPaidForPlayer = mostPaidForPlayer;
         return m;
     }
@@ -187,9 +238,9 @@ public class sportQuiz {
 
 }
 class Teams {
-    int faCups; //12 963
+    int faCups;
     int mostGoalScored;
-    int mostPaidForPlayer;
+    String mostPaidForPlayer;
     String mostGoalScorer;
     String playerMostAppearances;
     String mostExpensiveTransferedPlayer;
